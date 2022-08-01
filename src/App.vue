bb788744620aaf1c90b404bbc7096e7efdab20ef
罗恒(luo_h603) <715493349@qq.com>
2022-08-01 20:17:19 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <!--
  * @Author: luo_h603
  * @Date: 2022-07-21 11:30:33
- * @LastEditTime: 2022-07-21 14:03:43
+ * @LastEditTime: 2022-08-01 20:17:08
  * @LastEditors: luo_h603
  * @Description:
  * God help those who help themselves
 -->
 <template>
   <div>app根组件</div>
+  <button>按钮</button>
 </template>
 
 <style lang="less"></style>
